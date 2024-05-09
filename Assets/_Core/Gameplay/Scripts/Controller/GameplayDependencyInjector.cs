0b7cd73812e5021e4d669bae5b7e221d08d83523
Haider-Ali-Sablo <haiderali@sablostudio.com>
2024-05-09 14:37:05 +0500

    -Add modules to dependency injector

--- Assets/_Core/Gameplay/Scripts/Controller/GameplayDependencyInjector.cs
+++ Assets/_Core/Gameplay/Scripts/Controller/GameplayDependencyInjector.cs
@@ -1,10 +1,18 @@
+using Sablo.Gameplay.Shape;
+using UnityEngine;
+using Grid = Sablo.Gameplay.Grid.Grid;
+
 namespace Sablo.Core
 {
     public class GameplayDependencyInjector : BaseDependencyInjector
     {
+        [SerializeField] private Tray _tray;
+        [SerializeField] private Grid _grid;
+        
         public override void InjectDependencies()
         {
-            
+            _tray.GridHandler = _grid;
+            _grid.TrayHandler = _tray;
         }
     }
 }
