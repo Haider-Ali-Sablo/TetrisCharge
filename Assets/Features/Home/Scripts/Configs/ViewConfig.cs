474b06429833d2514024a59a9d5418761bfff351
HaroonAmir <haroonamir@sablostudio.com>
2024-05-10 16:42:11 +0500

    Setup home module, Implement home play button, Setup basic UI

---
+++ Assets/Features/Home/Scripts/Configs/ViewConfig.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public partial class ViewConfig
+{
+    [Header("Home View")]
+    public float HomeFillDelta;
+    public float HomeBarthreshold;
+}
---
+++ Assets/Features/Home/Scripts/Interface/IHomeView.cs
@@ -0,0 +1,8 @@
+using System;
+namespace Sablo.Home
+{
+    public interface IHomeView
+    {
+    }
+
+}
