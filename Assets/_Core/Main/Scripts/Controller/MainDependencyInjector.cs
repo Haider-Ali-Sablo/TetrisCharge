b5fc49a39fcb70cdacc00731392ba82ae75a3989
HaroonAmir <haroonamir@sablostudio.com>
2024-05-10 16:35:31 +0500

    added references of transition, homeController in MainDependencyInjector

--- Assets/_Core/Main/Scripts/Controller/MainDependencyInjector.cs
+++ Assets/_Core/Main/Scripts/Controller/MainDependencyInjector.cs
@@ -1,10 +1,18 @@
+using Sablo.Home;
+using Sablo.UI.Transition;
+using UnityEngine;
+
 namespace Sablo.Core
 {
     public class MainDependencyInjector : BaseDependencyInjector
     {
+        [SerializeField] private Transition _transition;
+        [SerializeField] private HomeController _homeController;
+
         public override void InjectDependencies()
         {
             base.InjectDependencies();
+            _homeController.TransitionHandler = _transition;
         }
         
     }
